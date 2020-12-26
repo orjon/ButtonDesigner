e81565703147494268e34a68cfd56f843338d275
orjon <github@orjon.com>
2020-12-26 16:28:09 +0100

    Add accordion

--- src/App.js
+++ src/App.js
@@ -11,19 +11,15 @@ const App = (buttonValues) => {
   console.log(store);
   return (
     <Provider store={store}>
-      <div className='App'>
-        <div className='flexColumnCenter'>
-          <div className='buttonMaker'>
-            <header>
-              <h1>Button Maker</h1>
-            </header>
+      <div className='ButtonMaker'>
+        <header>
+          <h1>Button Maker</h1>
+        </header>
 
-            <main>
-              <Adjustments />
-              <ButtonPreview />
-            </main>
-          </div>
-        </div>
+        <main>
+          <Adjustments />
+          <ButtonPreview />
+        </main>
       </div>
     </Provider>
   );
--- src/Components/Adjustments.js
+++ src/Components/Adjustments.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import { Accordion, AccordionItem } from 'react-light-accordion';
 import RangeSlider from './RangeSlider';
 import '../styles/Adjustments.scss';
 
@@ -11,88 +12,92 @@ const Adjustments = () => {
   return (
     <div className='adjustments'>
       <h3>Adjustments</h3>
+      <Accordion atomic={false}>
+        <AccordionItem title='Button Size'>
+          <section className='adjustSection adjustSize'>
+            <RangeSlider
+              field='height'
+              percentageOption={false}
+              initial={30}
+              min={1}
+              max={200}
+            />
+            <RangeSlider
+              field='width'
+              percentageOption={false}
+              initial={100}
+              min={1}
+              max={500}
+            />
+          </section>
+        </AccordionItem>
+        <AccordionItem title='Corner Radii'>
+          <section className='adjustSection adjustSize'>
+            <form className='adjustmentContainer'>
+              <div className='checkbox'>
+                <input
+                  style={{ verticalAlign: 'bottom' }}
+                  type='checkbox'
+                  id='percentage'
+                  name='usePercentage'
+                  checked={lockCorners}
+                  onChange={handleLockCorners}
+                />
+                <label htmlFor='percentage'>&nbsp;Lock corners</label>
+              </div>
+            </form>
+            {lockCorners ? (
+              <RangeSlider
+                field='borderRadius'
+                percentageOption={true}
+                initial={50}
+                min={0}
+                max={500}
+              />
+            ) : (
+              <Fragment>
+                <RangeSlider
+                  field='borderRadiusTopLeft'
+                  percentageOption={true}
+                  initial={50}
+                  min={0}
+                  max={500}
+                />
+                <RangeSlider
+                  field='borderRadiusTopRight'
+                  percentageOption={true}
+                  initial={50}
+                  min={0}
+                  max={500}
+                />
+                <RangeSlider
+                  field='borderRadiusBottomRight'
+                  percentageOption={true}
+                  initial={50}
+                  min={0}
+                  max={500}
+                />
+                <RangeSlider
+                  field='borderRadiusBottomLeft'
+                  percentageOption={true}
+                  initial={50}
+                  min={0}
+                  max={500}
+                />
+              </Fragment>
+            )}
+          </section>
+        </AccordionItem>
 
-      <section className='adjustSection adjustSize'>
-        <h4>Button Size</h4>
-        <RangeSlider
-          field='height'
-          percentageOption={false}
-          initial={30}
-          min={1}
-          max={200}
-        />
-        <RangeSlider
-          field='width'
-          percentageOption={false}
-          initial={100}
-          min={1}
-          max={500}
-        />
-      </section>
-      <section className='adjustSection adjustSize'>
-        <h4>Corner Radii</h4>
-        <form className='adjustmentContainer'>
-          <div className='checkbox'>
-            <input
-              style={{ verticalAlign: 'bottom' }}
-              type='checkbox'
-              id='percentage'
-              name='usePercentage'
-              checked={lockCorners}
-              onChange={handleLockCorners}
-            />
-            <label htmlFor='percentage'>&nbsp;Lock corners</label>
-          </div>
-        </form>
-        {lockCorners ? (
-          <RangeSlider
-            field='borderRadius'
-            percentageOption={true}
-            initial={50}
-            min={0}
-            max={500}
-          />
-        ) : (
-          <Fragment>
-            <RangeSlider
-              field='borderRadiusTopLeft'
-              percentageOption={true}
-              initial={50}
-              min={0}
-              max={500}
-            />
-            <RangeSlider
-              field='borderRadiusTopRight'
-              percentageOption={true}
-              initial={50}
-              min={0}
-              max={500}
-            />
-            <RangeSlider
-              field='borderRadiusBottomRight'
-              percentageOption={true}
-              initial={50}
-              min={0}
-              max={500}
-            />
-            <RangeSlider
-              field='borderRadiusBottomLeft'
-              percentageOption={true}
-              initial={50}
-              min={0}
-              max={500}
-            />
-          </Fragment>
-        )}
-      </section>
-      <section className='adjustSection adjustSize'>
-        <h4>Text</h4>
-        <RangeSlider field='textSize' initial={30} min={1} max={50} />
-      </section>
-      <section className='adjustSection adjustSize'>
-        <h4>Margin &amp; Padding</h4>
-        <RangeSlider field='textSize' initial={30} min={1} max={50} />
-      </section>
+        <section className='adjustSection adjustSize'>
+          <h4>Text</h4>
+          <RangeSlider field='textSize' initial={30} min={1} max={50} />
+        </section>
+        <section className='adjustSection adjustSize'>
+          <h4>Margin &amp; Padding</h4>
+          <RangeSlider field='textSize' initial={30} min={1} max={50} />
+        </section>
+      </Accordion>
     </div>
   );
 };
